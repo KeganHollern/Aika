50cd08b6ab712b8febb062662aa17c8b4d4919f2
Kegan Hollern <keganhollern@gmail.com>
2023-08-07 22:06:56 -0500

    youtube search fixes

--- actions/youtube/search.go
+++ actions/youtube/search.go
@@ -4,8 +4,14 @@ import (
 	"aika/discord/discordai"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
 
-	yt "github.com/lithdew/youtube"
+	"github.com/buger/jsonparser"
+
 	"github.com/sashabaranov/go-openai"
 	"github.com/sashabaranov/go-openai/jsonschema"
 )
@@ -20,9 +26,8 @@ var (
 )
 
 type youtubeResult struct {
-	Title       string `json:"title"`
-	URL         string `json:"url"`
-	Description string `json:"description"`
+	Title string `json:"title"`
+	URL   string `json:"url"`
 }
 type youtubeResults struct {
 	Results []youtubeResult `json:"results"`
@@ -61,18 +66,131 @@ func handler_SearchYoutube(msgMap map[string]interface{}) (string, error) {
 func action_SearchYoutube(input string) (youtubeResults, error) {
 	searchResults := youtubeResults{}
 
-	results, err := yt.Search("fuck you", 0)
+	results, err := search(input, 5)
 	if err != nil {
 		return searchResults, fmt.Errorf("failed to search youtube; %w", err)
 	}
 
-	for _, result := range results.Items[0:maxSearchResults] {
+	for _, result := range results[0:maxSearchResults] {
 		searchResults.Results = append(searchResults.Results, youtubeResult{
-			Title:       result.Title,
-			URL:         fmt.Sprintf("https://youtu.be/%s", result.ID),
-			Description: result.Description[0:128],
+			Title: result.Title,
+			URL:   result.URL,
 		})
 	}
 
 	return searchResults, nil
+}
+
+type SearchResult struct {
+	Title, Uploader, URL, Duration, ID string
+	Live                               bool
+	SourceName                         string
+	Extra                              []string
+}
+
+func getContent(data []byte, index int) []byte {
+	id := fmt.Sprintf("[%d]", index)
+	contents, _, _, _ := jsonparser.Get(data, "contents", "twoColumnSearchResultsRenderer", "primaryContents", "sectionListRenderer", "contents", id, "itemSectionRenderer", "contents")
+	return contents
+}
+
+var httpClient = &http.Client{}
+
+func search(searchTerm string, limit int) ([]*SearchResult, error) {
+	results := []*SearchResult{}
+	url := fmt.Sprintf("https://www.youtube.com/results?search_query=%s", url.QueryEscape(searchTerm))
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build GET request; %w", err)
+	}
+	req.Header.Add("Accept-Language", "en")
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("cannot get youtube page; %w", err)
+	}
+
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	buffer, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read body; %w", err)
+	}
+
+	body := string(buffer)
+	splittedScript := strings.Split(body, `window["ytInitialData"] = `)
+	if len(splittedScript) != 2 {
+		splittedScript = strings.Split(body, `var ytInitialData = `)
+	}
+
+	if len(splittedScript) != 2 {
+		return nil, fmt.Errorf("too many splitted scripts")
+	}
+	splittedScript = strings.Split(splittedScript[1], `window["ytInitialPlayerResponse"] = null;`)
+	jsonData := []byte(splittedScript[0])
+
+	index := 0
+	var contents []byte
+
+	for {
+		contents = getContent(jsonData, index)
+		_, _, _, err = jsonparser.Get(contents, "[0]", "carouselAdRenderer")
+
+		if err == nil {
+			index++
+		} else {
+			break
+		}
+	}
+
+	_, err = jsonparser.ArrayEach(contents, func(value []byte, t jsonparser.ValueType, i int, err error) {
+		if err != nil { // error so just skip this item
+			return
+		}
+
+		if limit > 0 && len(results) >= limit {
+			return
+		}
+
+		id, err := jsonparser.GetString(value, "videoRenderer", "videoId")
+		if err != nil {
+			return
+		}
+
+		title, err := jsonparser.GetString(value, "videoRenderer", "title", "runs", "[0]", "text")
+		if err != nil {
+			return
+		}
+
+		uploader, err := jsonparser.GetString(value, "videoRenderer", "ownerText", "runs", "[0]", "text")
+		if err != nil {
+			return
+		}
+
+		live := false
+		duration, err := jsonparser.GetString(value, "videoRenderer", "lengthText", "simpleText")
+
+		if err != nil {
+			duration = ""
+			live = true
+		}
+
+		results = append(results, &SearchResult{
+			Title:      title,
+			Uploader:   uploader,
+			Duration:   duration,
+			ID:         id,
+			URL:        fmt.Sprintf("https://youtube.com/watch?v=%s", id),
+			Live:       live,
+			SourceName: "youtube",
+		})
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse results; %w", err)
+	}
+
+	return results, nil
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ go 1.19
 
 require (
 	github.com/aws/aws-sdk-go v1.44.306
+	github.com/buger/jsonparser v1.1.1
 	github.com/bwmarrin/discordgo v0.27.1
 	github.com/gocolly/colly v1.2.0
 	github.com/google/uuid v1.3.0
-	github.com/lithdew/youtube v0.0.0-20200524132213-a1fd5d688f8a
 	github.com/sashabaranov/go-openai v1.14.1
 	github.com/sirupsen/logrus v1.9.3
 	gopkg.in/yaml.v2 v2.4.0
@@ -25,18 +25,15 @@ require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/klauspost/compress v1.10.4 // indirect
-	github.com/lithdew/bytesutil v0.0.0-20200409052507-d98389230a59 // indirect
-	github.com/lithdew/nicehttp v0.0.0-20200422123956-0d3d3dd9b482 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.12.0 // indirect
-	github.com/valyala/fastjson v1.5.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
